9dfc1b5c5627f1522a49c8fd5bb21c6d941aa8d8
awbreemer <abreemer@zagmail.gonzaga.edu>
2023-02-01 17:00:45 -0800

    Done!

--- MoodyPart2.jl
+++ MoodyPart2.jl
@@ -1,36 +1,17 @@
+#Andrew Breemer
+#Moody Diagram
+
 using Plots
 using Printf
-
-ReVals = 10 .^range(3.4,8,400)
-realativeRoughnessVals = 10 .^range(-6,-1.3,15)
-
-#=
-fric_Factor = [FixedPointIteration(Re, realativeRoughness, .05) for realativeRoughness in realativeRoughnessVals, Re in ReVals]
-
-pltMoody = plot(ReVals, fric_Factor', xaxis = :log10, yaxis = :log10)
-=#
-
-for i in range(1,length(realativeRoughnessVals))
-    global frictionFactorVals = Array{Float64}(undef, length(ReVals))
-    for j in range(1,length(ReVals))
-        frictionFactorVals[j] = FixedPointIteration(ReVals[j], realativeRoughnessVals[i], .1)
-
-    end
-    if i == 1
-        global plt = plot(ReVals, frictionFactorVals, xaxis =:log10, yaxis = :log10,legendfont = 6, grid = true, xlabel = "Reynolds Number", ylabel = "Friction Factor", legend = :bottomleft, label = @sprintf("Real. Rough. = %e", realativeRoughnessVals[i]))
-    else
-        plot!(ReVals,frictionFactorVals, label = @sprintf("Real. Rough. = %e", realativeRoughnessVals[i]))
-    end
-end
 
 
 function FixedPointIteration(Re, RealativeRoughness, firstGuess)
     
-    tolerance = 0.0001
+    tolerance = 0.0001 #here is how close the iteration has to the previous iteration
     stopFlag = false
-    x = GofX(1 / sqrt(firstGuess), Re, RealativeRoughness)
+    x = GofX(1 / sqrt(firstGuess), Re, RealativeRoughness) #the first value 
     previous = 0.0
-    while !stopFlag
+    while !stopFlag #complete fixed point iteration untill within tolerance
         realativeChange = abs(1 / x^2 - 1 / x^2)
 
         if realativeChange < tolerance
@@ -40,15 +21,36 @@ function FixedPointIteration(Re, RealativeRoughness, firstGuess)
         previous = x
         x = previous + GofX(previous, Re, RealativeRoughness) 
 
-        #println("x is ", x, "  and the ff is ", 1 / sqrt(x))
     end
 
     return(1/x^2)
 
 end
 
+#Seperate function for simplicity for GofX for the Colbrook equation
 function GofX(x, Re, RealativeRoughness)
     return(-2*log10((RealativeRoughness/3.7)+((2.51/Re)*x)))
 end
 
+#Set vals for the Re and for the Realative Roughness
+ReVals = 10 .^range(3.4,8,400)
+realativeRoughnessVals = 10 .^range(-6,-1.3,15)
+#=
+Nested for loop. One loop for running through the different roughness values and one for each of the points for Re. Utilizes the 
+fixed point iteration for each loop. After all the friction factor values have been calculated for a given roughness a line on the 
+plot is created. 
+=#
+for i in range(1,length(realativeRoughnessVals)) #For the roughness values
+    global frictionFactorVals = Array{Float64}(undef, length(ReVals)) #re-initialize empty array for storing friction factor vals
+    for j in range(1,length(ReVals)) #For each friction factor value
+        frictionFactorVals[j] = FixedPointIteration(ReVals[j], realativeRoughnessVals[i], .1)
+
+    end
+    if i == 1 #there is most likely a more elegent meathod for plotting, but this worked
+        global plt = plot(ReVals, frictionFactorVals, xaxis =:log10, yaxis = :log10,legendfont = 6, grid = true, xlabel = "Reynolds Number", ylabel = "Friction Factor", legend = :bottomleft, label = @sprintf("Real. Rough. = %e", realativeRoughnessVals[i]))
+    else
+        plot!(ReVals,frictionFactorVals, label = @sprintf("Real. Rough. = %e", realativeRoughnessVals[i]))
+    end
+end
+savefig(plt, "myfig.png")
 
--- Sine.jl
+++ Sine.jl
@@ -1,13 +1,9 @@
+#Andrew Breemer
+#Sine HW
+
 using Pkg
 using BenchmarkTools
 
-
-
-function SineTest(x, realativeChange)
-    @benchmark Sine(x, realativeChange)
-    @benchmark sin(x)
-
-end
 
 function Sine(x, realativeChange)
     # shift x to a value between 0 and 2*pi
@@ -39,32 +35,42 @@ end
 ## accounts for negative inputs as well
 function TwoPiShift(x)
     if 2*pi < x 
+        
         while x > 2*pi
             x -= 2*pi
         end
+
     elseif x < 0
+
         while x < 0 
             x += 2*pi
         end
+
     end
+
         return(x)
 end
 
 function myFactorial(n)
     nextMultiplier = n-1
-    ##println( "next multiplier is ", nextMultiplier)
+
     while nextMultiplier >= 2
-        ##println("iterated n = ", n, "  |  nextMultiplier = ", nextMultiplier)
         n *= nextMultiplier
         nextMultiplier -= 1
     end
+
     return(n)
 end
 
 function myExponential(body, power)
     newBody = 1
+
     for i in 1:power
         newBody *= body
     end
+
     return(newBody)
 end
+
+@benchmark Sine(22, .001)
+@benchmark sin(22)
